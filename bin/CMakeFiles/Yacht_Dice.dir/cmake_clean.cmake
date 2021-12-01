da8887d0436971b064139f616174e212d60d4840
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2021-12-01 11:42:25 +0900

    init

---
+++ bin/CMakeFiles/Yacht_Dice.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Yacht_Dice.dir/main.cpp.o"
+  "CMakeFiles/Yacht_Dice.dir/main.cpp.o.d"
+  "Yacht_Dice"
+  "Yacht_Dice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Yacht_Dice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ bin/CMakeFiles/Yacht_Dice.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Yacht_Dice.
+# This may be replaced when dependencies are built.
