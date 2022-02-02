2daec4181851f1c60958e786c844e85e1b7e8110
cogus705 <cogus705@naver.com>
2022-02-02 17:34:32 +0900

    스코어보드 display함수 테스트케이스 추가

--- tests/scoreboardTest.cpp
+++ tests/scoreboardTest.cpp
@@ -52,3 +52,73 @@ TEST(scoreboardTest, displayTest6) { //4 of kind
                             18,18,0,0,0,0};
     EXPECT_EQ(v,res);
 }
+
+TEST(scoreboardTest, displayTest7) { //3 야추
+    scoreboard * a = new scoreboard();
+    dice_game _dice({3,3,3,3,3});
+    auto v = a->display(_dice);
+    std::vector<int> res = {0,0,15,0,0,0,0,
+                            15,15,0,0,0,50};
+    EXPECT_EQ(v,res);
+}
+TEST(scoreboardTest, displayTest8) { //4 야추
+    scoreboard * a = new scoreboard();
+    dice_game _dice({4,4,4,4,4});
+    auto v = a->display(_dice);
+    std::vector<int> res = {0,0,0,20,0,0,0,
+                            20,20,0,0,0,50};
+    EXPECT_EQ(v,res);
+}
+TEST(scoreboardTest, displayTest9) { //5 야추
+    scoreboard * a = new scoreboard();
+    dice_game _dice({5,5,5,5,5});
+    auto v = a->display(_dice);
+    std::vector<int> res = {0,0,0,0,25,0,0,
+                            25,25,0,0,0,50};
+    EXPECT_EQ(v,res);
+}
+
+TEST(scoreboardTest, displayTest10) { //6 야추
+    scoreboard * a = new scoreboard();
+    dice_game _dice({6,6,6,6,6});
+    auto v = a->display(_dice);
+    std::vector<int> res = {0,0,0,0,0,30,0,
+                            30,30,0,0,0,50};
+    EXPECT_EQ(v,res);
+}
+
+TEST(scoreboardTest, displayTest11) {
+    scoreboard * a = new scoreboard();
+    dice_game _dice({1,6,6,6,5});
+    auto v = a->display(_dice);
+    std::vector<int> res = {1,0,0,0,5,18,0,
+                            24,0,0,0,0,0};
+    EXPECT_EQ(v,res);
+}
+
+TEST(scoreboardTest, displayTest12) { //4 of kind
+    scoreboard * a = new scoreboard();
+    dice_game _dice({5,6,6,6,6});
+    auto v = a->display(_dice);
+    std::vector<int> res = {0,0,0,0,5,24,0,
+                            29,29,0,0,0,0};
+    EXPECT_EQ(v,res);
+}
+
+TEST(scoreboardTest, displayTest13) { //풀하우스
+    scoreboard * a = new scoreboard();
+    dice_game _dice({2,2,4,4,4});
+    auto v = a->display(_dice);
+    std::vector<int> res = {0,4,0,12,0,0,0,
+                            16,0,16,0,0,0};
+    EXPECT_EQ(v,res);
+}
+
+TEST(scoreboardTest, displayTest14) { //스몰 스트레이트
+    scoreboard * a = new scoreboard();
+    dice_game _dice({2,2,3,4,5});
+    auto v = a->display(_dice);
+    std::vector<int> res = {0,4,3,4,5,0,0,
+                            16,0,0,15,0,0};
+    EXPECT_EQ(v,res);
+}
